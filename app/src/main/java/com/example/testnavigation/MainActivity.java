c463678d852f5b9e435b49cf4f345602e59260cd
Changshin Lee <iasandcb@gmail.com>
2016-03-27 12:32:49 +0900

    add default home fragment

--- app/src/main/java/com/example/testnavigation/MainActivity.java
+++ app/src/main/java/com/example/testnavigation/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity
         DetailFragment.OnFragmentInteractionListener,
         CreditFragment.OnFragmentInteractionListener,
         InfoFragment.OnFragmentInteractionListener,
+        HomeFragment.OnFragmentInteractionListener,
         FragmentManager.OnBackStackChangedListener {
 
     ActionBarDrawerToggle toggle;
@@ -92,6 +93,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, new HomeFragment());
+        ft.commit();
     }
 
     @Override
